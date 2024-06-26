b77d68656273a12249ea45e71324aadbd418cfa4
soufainos <mazouara97@gmail.com>
2024-06-26 13:07:00 +0100

    Update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,22 +2,22 @@ def reminder():
     task = input("Enter your task: ")
     priority = input("Priority (high/medium/low): ").lower()
     time_bound = input("Is it time-bound? (yes/no): ").lower()
-
+    sensitive_reminder = f"Note: {task} is a {priority} priority task. Consider completing it when you have free time."
+    reminder = f"Note: {task} is a {priority} priority task. Consider completing it when you have free time."
     match priority:
         case "high": 
             if time_bound == "yes":
-                print(f"Reminder: {task} is a {priority} priority task that requires immediate attention today!")
-            elif time_bound == "no":
-                print(f"Note: {task} is a {priority} priority task. Consider completing it when you have free time.")
+                print(sensitive_reminder)
+            else:
+                print(reminder)
         case "medium": 
             if time_bound == "yes":
-                print(f"Reminder: {task} is a {priority} priority task that requires immediate attention today!")
-            elif time_bound == "no":
-                print(f"Note: {task} is a {priority} priority task. Consider completing it when you have free time.")
+                print(sensitive_reminder)
+            else:
+                print(reminder)
         case "low": 
             if time_bound == "yes":
-                print(f"Reminder: {task} is a {priority} priority task that requires immediate attention today!")
-            elif time_bound == "no":
-                print(f"Note: {task} is a {priority} priority task. Consider completing it when you have free time.")
+                print(sensitive_reminder)
+            else:
+                print(reminder)
 reminder()
-
