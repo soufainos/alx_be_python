41305fc3bd001762b74f6753289e64405894d572
soufainos <mazouara97@gmail.com>
2024-07-07 21:42:47 +0100

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -4,7 +4,7 @@ def perform_operation(num1, num2, operation):
     if operation == "subtract":
         return num1 - num2
     if operation == "multiply":
-        return num1 + num2
+        return num1 * num2
     if operation == "divide":
         if num2 == 0:
             return "can not divide by 0"
